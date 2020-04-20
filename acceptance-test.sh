02583417bbdada1b3d35c16933baff908207c6c5
demo-informatica <gheorghecater@gmail.com>
2020-04-20 08:08:04 +0300

    modify6

--- acceptance-test.sh
+++ acceptance-test.sh
@@ -4,4 +4,4 @@ set - x
 NODE_IP=$(kubectl get nodes -o jsonpath='{ $.items[0].status.addresses[?(@.type=="ExternalIP")].address }')
 NODE_PORT=$(kubectl get svc calculator-service -o=jsonpath='{.spec.ports[0].nodePort}')
 ./gradlew acceptanceTest -Dcalculator.url=http://${NODE_IP}:${NODE_PORT}
-  //test $(curl localhost:8765/suma? a=1 \& b=2 ) - eq 3
+  //test $(curl ec2-35-181-151-53.eu-west-3.compute.amazonaws.com:8765/suma? a=1 \& b=2 ) - eq 3
